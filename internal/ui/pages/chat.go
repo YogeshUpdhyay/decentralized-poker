f80bfb661c3c5866819975c5057fe31bc8cd7b46
YogeshUpdhyay <yogeshh2021@gmail.com>
2025-10-04 17:38:41 +0000

    add connection and connect views

--- internal/ui/pages/chat.go
+++ internal/ui/pages/chat.go
@@ -35,15 +35,15 @@ func (c *Chat) OnHide(_ context.Context) {
 
 func (c *Chat) Content(ctx context.Context) fyne.CanvasObject {
 	chat := container.NewHSplit(
-		getSideNav(ctx),
-		getChatLayout(ctx, nil),
+		c.getSideNav(ctx),
+		c.getChatLayout(ctx, nil),
 	)
 	chat.SetOffset(0.25)
 
 	return chat
 }
 
-func getSideNav(ctx context.Context) fyne.CanvasObject {
+func (c *Chat) getSideNav(ctx context.Context) fyne.CanvasObject {
 	// logo
 	logo := canvas.NewImageFromFile(constants.LogoPath)
 	logo.FillMode = canvas.ImageFillContain
@@ -62,8 +62,17 @@ func getSideNav(ctx context.Context) fyne.CanvasObject {
 		}
 
 		// display a share link or qr code in the dailog
-		getConnectionPopUp(ctx, qrPath, myAddresses)
+		c.getConnectionPopUp(ctx, qrPath, myAddresses)
 	})
+
+	// new chat button
+	newChatButton := widget.NewButtonWithIcon(
+		"New Chat",
+		theme.ContentAddIcon(),
+		func() {
+			c.getAddNewChatPopUp(ctx)
+		},
+	)
 
 	// chat threads list
 	chatThreads := container.NewVBox()
@@ -79,20 +88,14 @@ func getSideNav(ctx context.Context) fyne.CanvasObject {
 				),
 			),
 			container.NewPadded(
-				widget.NewButtonWithIcon(
-					"New Chat",
-					theme.ContentAddIcon(),
-					func() {
-						log.WithContext(ctx).Info("New chat button tapped")
-					},
-				),
+				newChatButton,
 			), nil, nil, chatThreads,
 		),
 		// chatThreads,
 	)
 }
 
-func getChatLayout(ctx context.Context, peerData *models.PeerData) fyne.CanvasObject {
+func (c *Chat) getChatLayout(ctx context.Context, peerData *models.PeerData) fyne.CanvasObject {
 	// if no peer data is provided, show a placeholder
 	if peerData == nil {
 		return container.NewCenter(
@@ -173,13 +176,30 @@ func getChatLayout(ctx context.Context, peerData *models.PeerData) fyne.CanvasOb
 	)
 }
 
-func getConnectionPopUp(ctx context.Context, qrPath string, myAddresses []string) {
+func (c *Chat) getConnectionPopUp(ctx context.Context, qrPath string, myAddresses []string) {
 	log.WithContext(ctx).Info("generating connection pop up")
 
+	// modal title
+	modalTitle := canvas.NewText("Share your address", color.White)
+	modalTitle.TextSize = 16
+	modalTitle.TextStyle.Bold = true
+	modalTitleContainer := container.NewPadded(container.NewCenter(modalTitle))
+
+	//connection addresses
+	connectionAddresses := container.NewPadded(widget.NewLabel(
+		strings.Join(myAddresses, "\n"),
+	))
+
+	qrImage := canvas.NewImageFromFile(qrPath)
+	qrImage.FillMode = canvas.ImageFillContain
+	qrImage.SetMinSize(fyne.NewSize(200, 200))
+	qrImageContainer := container.NewPadded(container.NewCenter(qrImage))
+
+	// Create the content for the pop-up
 	content := container.NewVBox(
-		widget.NewLabel("Your Node Addresses"),
-		widget.NewLabel(strings.Join(myAddresses, "\n")),
-		canvas.NewImageFromFile(qrPath),
+		modalTitleContainer,
+		connectionAddresses,
+		qrImageContainer,
 	)
 
 	// Create the pop-up
@@ -191,4 +211,86 @@ func getConnectionPopUp(ctx context.Context, qrPath string, myAddresses []string
 	content.Add(widget.NewButton("Close", func() { popUp.Hide() }))
 
 	popUp.Show()
+}
+
+func (c *Chat) getAddNewChatPopUp(ctx context.Context) {
+	log.WithContext(ctx).Info("generating add new chat pop up")
+
+	// modal title
+	modalTitle := canvas.NewText("Start a new chat", color.White)
+	modalTitle.TextSize = 16
+	modalTitle.TextStyle.Bold = true
+	modalTitleContainer := container.NewPadded(container.NewCenter(modalTitle))
+
+	// input field for peer ID or address
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter Peer ID or Address")
+	inputFieldDimesion := canvas.NewRectangle(color.Transparent)
+	inputFieldDimesion.SetMinSize(fyne.NewSize(300, 0))
+
+	// cta row
+	ctaRow := container.NewGridWithColumns(2)
+
+	// underlay container with a border
+	underLayContainer := canvas.NewRectangle(color.Transparent)
+	underLayContainer.SetMinSize(fyne.NewSize(350, 220))
+
+	// spacer rect
+	spacerRect := canvas.NewRectangle(color.Transparent)
+	spacerRect.SetMinSize(fyne.NewSize(0, 40))
+
+	// create the content for the pop-up
+	content := container.NewPadded(
+		underLayContainer,
+		container.NewCenter(container.NewVBox(
+			modalTitleContainer,
+			spacerRect,
+			container.NewPadded(input, inputFieldDimesion),
+			spacerRect,
+			ctaRow,
+		)),
+	)
+
+	// Create the pop-up
+	popUp := widget.NewModalPopUp(
+		content,
+		fyne.CurrentApp().Driver().AllWindows()[0].Canvas(),
+	)
+
+	cancelBtn := widget.NewButton("Cancel", func() { popUp.Hide() })
+	cancelBtn.Importance = widget.MediumImportance
+	ctaRow.Add(cancelBtn)
+
+	connectBtn := widget.NewButton("Connect", func() {
+		address := strings.TrimSpace(input.Text)
+		if address != "" {
+			log.WithContext(ctx).Infof("Starting chat with peer ID: %s", address)
+			// logic to initiate chat with the given peer ID goes here
+			err := c.connectToPeer(ctx, address)
+			if err != nil {
+				log.WithContext(ctx).WithError(err).Errorf("error connecting to peer %s", address)
+			}
+			popUp.Hide()
+		}
+	})
+	connectBtn.Importance = widget.HighImportance
+	ctaRow.Add(connectBtn)
+
+	popUp.Show()
+}
+
+func (c *Chat) connectToPeer(ctx context.Context, address string) error {
+	server := p2p.GetServer()
+	if server == nil {
+		return nil
+	}
+
+	err := server.Connect(address)
+	if err != nil {
+		log.WithContext(ctx).WithError(err).Errorf("error connecting to peer %s", address)
+		return err
+	}
+
+	log.WithContext(ctx).Infof("successfully connected to peer %s", address)
+	return nil
 }
